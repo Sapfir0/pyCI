f57bded07aa3022b6ca23558a1bed1eabe7d15eb
Sapfir0 <sapfir999999@yandex.ru>
2019-04-27 20:58:26 +0400

    puk2

--- pyCi.py
+++ pyCi.py
@@ -16,11 +16,41 @@ class ExampleApp(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
         self.setupUi(self)  # Это нужно для инициализации нашего дизайна
         self.changeDir.clicked.connect(self.browseFolder)
         self.changeDir2.setVisible(False)
+
+        self.changeDir2.clicked.connect(self.browseFolder)
+
+        self.tests.currentIndexChanged.connect(self.testing)
+        self.complete.setPixmap(QPixmap(":/images/icons8-cancel-16.png"))
+        self.arrow.setPixmap(QPixmap(":/images/icons8-right-16.png"))
         
     def browseFolder(self):
         directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Select project folder")
-        self.dir.setText(directory)
-        self.complete.setPixmap(QPixmap(":/images/icons8-checked-16.png"))
+        def complete(self, var): #не робит как нужно
+            self.dir.setText(directory)
+            self.changeDir2.setVisible(var)
+            self.changeDir.setVisible(not var)
+            if (var == True):   
+                self.complete.setPixmap(QPixmap(":/images/icons8-checked-16.png"))
+            else: 
+                self.complete.setPixmap(QPixmap(":/images/icons8-cancel-16.png"))
+            self.dir.setEnabled(not var)
+        
+        if (directory != ''):
+            complete(self, True)
+        else:
+            complete(self, False)
+
+
+
+
+    def testing(self): 
+        if ( self.tests.currentText() == "Другое"):
+            self.findedTest.setText("У нас нет тестов для тебя")
+            self.goToTestBtn.setVisible(False) 
+        elif ( self.tests.currentText() == "Node.js"):
+            self.findedTest.setText("У нас есть тесты для тебя")
+            self.goToTestBtn.setVisible(True) 
+    
 
 
 def main():
